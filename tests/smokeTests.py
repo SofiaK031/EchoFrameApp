92c33b260636c0c06572c22a66127e332141028b
Sofia K <119862637+SofiaK031@users.noreply.github.com>
2025-03-08 22:14:29 +0200

    Update smokeTests.py

--- tests/smokeTests.py
+++ tests/smokeTests.py
@@ -2,16 +2,18 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # локальний шлях до файлів
 SUBSCRIPTIONS_PAGE = f"file://{PROJECT_ROOT}/header-menu/subscriptions.html"
 INDEX_PAGE = f"file://{PROJECT_ROOT}/index.html"
 
-
 # 1. Позитивний тест: Оформлення підписки
 def test_successful_subscription():
     driver = webdriver.Chrome()
     driver.get(SUBSCRIPTIONS_PAGE)
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "faculty")))
     faculty_dropdown = Select(driver.find_element(By.ID, "faculty"))
     faculty_dropdown.select_by_visible_text("Факультет комп’ютерних наук")
     category_dropdown = Select(driver.find_element(By.ID, "category"))
