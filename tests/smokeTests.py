ef2e536dc2a73ffffe3010ea87321121d10afd2c
Sofia K <119862637+SofiaK031@users.noreply.github.com>
2025-03-08 23:00:21 +0200

    Update smokeTests.py

--- tests/smokeTests.py
+++ tests/smokeTests.py
@@ -2,8 +2,8 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # локальний шлях до файлів
 
@@ -21,11 +21,11 @@ def test_successful_subscription():
 
     # # Очікування завантаження випадаючого списку "faculty"
     # WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "faculty")))
-    WebDriverWait(driver, 10).until(lambda d: d.execute_script("return document.readyState") == "complete")
-    # Очікуємо елемент у DOM
-    faculty_dropdown = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "faculty"))
-    )
+    # WebDriverWait(driver, 10).until(lambda d: d.execute_script("return document.readyState") == "complete")
+    # # Очікуємо елемент у DOM
+    # faculty_dropdown = WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located((By.ID, "faculty"))
+    # )
 
     faculty_dropdown = Select(driver.find_element(By.ID, "faculty"))
     faculty_dropdown.select_by_visible_text("Факультет комп'ютерних наук")
@@ -33,8 +33,8 @@ def test_successful_subscription():
     category_dropdown.select_by_visible_text("Працевлаштування")
     driver.find_element(By.ID, "submitSubscription").click()
 
-    # Очікуємо появи alert
-    WebDriverWait(driver, 5).until(EC.alert_is_present())
+    # # Очікуємо появи alert
+    # WebDriverWait(driver, 5).until(EC.alert_is_present())
     alert = driver.switch_to.alert
     assert "Підписку оформлено!" in alert.text
     alert.accept()
